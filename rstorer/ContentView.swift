cfea509fade94dcca4bfc8990000ea314b329d8a
bill donner <billdonner@gmail.com>
2023-08-26 08:07:26 -0400

    Initial Commit

---
+++ rstorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rstorer
+//
+//  Created by bill donner on 8/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rstorer/rstorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  rstorerApp.swift
+//  rstorer
+//
+//  Created by bill donner on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct rstorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
